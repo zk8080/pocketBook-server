95a108fa8f9007e2d2f70f7bc6becb6ea999d185
zk8080 <zhangkang8080@163.com>
2021-11-18 17:16:15 +0800

    feat: 用户登录接口，token鉴权

--- app/controller/user.js
+++ app/controller/user.js
@@ -5,6 +5,8 @@ const Controller = require('egg').Controller;
 const defaultAvatar = 'http://s.yezgea02.com/1615973940679/WeChat77d6d2ac093e247c361f0b8a7aeb6c2a.png';
 
 class UserController extends Controller {
+
+  // 注册
   async register() {
     const { ctx } = this;
     const { username, password } = ctx.request.body; // 获取注册需要的参数
@@ -20,7 +22,6 @@ class UserController extends Controller {
 
     // 验证数据库内是否已经有该账户名
     const userInfo = await ctx.service.user.getUserByName(username);
-    console.log(userInfo, '--userInfo--');
     // 判断是否已经存在
     if (userInfo && userInfo.id) {
       ctx.body = {
@@ -53,6 +54,66 @@ class UserController extends Controller {
         data: null,
       };
     }
+  }
+
+  // 登录
+  async login() {
+    // app 为全局属性，相当于所有的插件方法都植入到了 app 对象。
+    const { ctx, app } = this;
+    const { username, password } = ctx.request.body;
+    // 根据用户名，在数据库查找相对应的id操作
+    const userInfo = await ctx.service.user.getUserByName(username);
+    // 没找到说明没有该用户
+    if (!userInfo || !userInfo.id) {
+      ctx.body = {
+        code: 500,
+        msg: '账号不存在',
+        data: null,
+      };
+      return;
+    }
+    // 找到用户，并且判断输入密码与数据库中用户密码。
+    if (userInfo && password !== userInfo.password) {
+      ctx.body = {
+        code: 500,
+        msg: '账号密码错误',
+        data: null,
+      };
+      return;
+    }
+
+    // 生成 token 校验
+    // app.jwt.sign 方法接受两个参数，第一个为对象，对象内是需要加密的内容；第二个是加密字符串，上文已经提到过。
+    const token = app.jwt.sign({
+      id: userInfo.id,
+      username: userInfo.username,
+      exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60), // token 有效期为 24 小时
+    }, app.config.jwt.secret);
+
+    ctx.body = {
+      code: 200,
+      message: '登录成功',
+      data: {
+        token,
+      },
+    };
+  }
+
+  // 验证
+  async verify() {
+    const { ctx, app } = this;
+    // 通过 token 解析，拿到 user_id
+    const token = ctx.request.header.authorization; // 请求头获取 authorization 属性，值为 token
+    // 通过 app.jwt.verify + 加密字符串 解析出 token 的值
+    const decode = await app.jwt.verify(token, app.config.jwt.secret);
+    // 响应接口
+    ctx.body = {
+      code: 200,
+      message: '获取成功',
+      data: {
+        ...decode,
+      },
+    };
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -4,11 +4,14 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
+  const { router, controller, middleware } = app;
+  const _jwt = middleware.jwtErr(app.config.jwt.secret);
   router.get('/', controller.home.index);
   router.get('/user', controller.home.user);
   router.post('/add_user', controller.home.addUser);
   router.post('/edit_user', controller.home.editUser);
   router.post('/delete_user', controller.home.deleteUser);
   router.post('/api/user/register', controller.user.register);
+  router.post('/api/user/login', controller.user.login);
+  router.get('/api/user/verify', _jwt, controller.user.verify);
 };
